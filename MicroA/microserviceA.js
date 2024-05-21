eda7f104f43548f0c108d32123bf5f861267ff3a
WaltzOT <123332116+WaltzOT@users.noreply.github.com>
2024-05-20 20:56:13 -0700

    Microservice A complete

--- MicroA/microserviceA.js
+++ MicroA/microserviceA.js
@@ -2,17 +2,36 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const cities = [
-    { name: "Corvallis, OR", timezone: "America/Los_Angeles" },
-    { name: "New York, NY", timezone: "America/New_York" },
-    { name: "London, UK", timezone: "Europe/London" }
-    // Add more cities and timezones here
-];
+app.use(express.json());
 
-app.get('/cities', (req, res) => {
-    res.json(cities);
+// Convert metric to imperial
+app.post('/metric-to-imperial', (req, res) => {
+    const gramsArray = req.body.grams 
+    const poundsArray = gramsArray.map(grams => grams * 0.00220462)
+    res.json({ pounds: poundsArray })
+})
+
+// Convert imperial to metric
+app.post('/imperial-to-metric', (req, res) => {
+    const poundsArray = req.body.pounds 
+    const gramsArray = poundsArray.map(pounds => pounds / 0.00220462) 
+    res.json({ grams: gramsArray })
+})
+
+// Convert metric to metric
+app.post('/metric-to-metric', (req, res) => {
+    const gramsArray = req.body.grams
+    const milligramsArray = gramsArray.map(grams => grams * 0.001)
+    res.json({ milligrams: milligramsArray })
+})
+
+// Convert imperial to imperial
+app.post('/imperial-to-imperial', (req, res) => {
+    const tbspArray = req.body.tablespoon
+    const tspArray = tbspArray.map(tablespoon => tablespoon * 3)
+    res.json({ teaspoons: tspArray })
 });
 
 app.listen(port, () => {
-    console.log(`City timezone service running on http://localhost:${port}`);
+    console.log(`Server running on http://localhost:${port}`);
 });
